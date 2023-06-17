1f8ca24e55b2eb6d1c1029a38fa2259aa0c7de5c
Gonzalo Giampietri <giampietri.gonzalo@gmail.com>
2023-06-17 08:01:48 -0300

    :wrench: (setup): Initial project files

---
+++ MarvelData/MarvelData/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  MarvelData
+//
+//  Created by Gonzalo.Giampietri on 17/06/2023.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ MarvelData/MarvelData/MarvelDataApp.swift
@@ -0,0 +1,17 @@
+//
+//  MarvelDataApp.swift
+//  MarvelData
+//
+//  Created by Gonzalo.Giampietri on 17/06/2023.
+//
+
+import SwiftUI
+
+@main
+struct MarvelDataApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
